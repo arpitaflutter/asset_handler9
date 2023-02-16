e5cafbb80aad99ad97b304414bb0007dd13e8aa5
arpitaflutter <116253518+arpitaflutter@users.noreply.github.com>
2023-02-16 16:31:36 +0530

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+import 'home.dart';
+
+void main()
+{
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: {
+        '/':(context) => home(),
+      },
+    ),
+  );
+}
